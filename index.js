97cd3ebc2bbc00dc9ff46291f75be98ab7800f9b
OctoMeep <roboneleven@gmail.com>
2018-05-11 18:52:41 +0200

    Fix everything.

--- index.js
+++ index.js
@@ -2,21 +2,8 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const fs = require("fs");
 const client = new Discord.Client();
-const maxPost = require("./maxPost.json");
 const imgs = require("./allImages.json");
 
-var currentID = maxPost.maxPostNum;
-var idName = "imgID-" + currentID;
-
-function writeMax() {
-	fs.writeFile("MaxPost.json", '{"maxPostNum": ' + currentID + '}', "utf8", (err)=>{
-		if (err) {
-			throw err;
-		}
-		currentID++;
-		idName = "imgID-" + currentID;
-	});
-}
 
 var session = {
 	active: false
@@ -25,54 +12,58 @@ var session = {
 client.login(config.token);
 
 client.on('ready', () => {
-  console.log('I am ready!');
-  client.channels.findAll("name", "general").forEach(channel=>{
-	  channel.send("Start Successful");
-  });
+	console.log('I am ready!');
+	client.channels.findAll("name", "general").forEach(channel => {
+		channel.send("Start Successful");
+	});
 });
 
 client.on('message', message => {
 	var args = message.content.split("~");
-	
-	if(message.author.bot) return;
-	if (message.channel.type === 'dm') {
-		console.log(message.contents);
-		switch(args[0]) {
-			case ">view":
-				switch(args[1]) {
-					case "":
-						channel.send(allImages[random(1, currentID)].URL);
+
+	if (message.author.bot) return;
+	fs.readFile('allImages.json', (err, data) => {
+		images = JSON.parse(data);
+		if (message.channel.type === 'dm') {
+			console.log(message.content);
+			switch (args[0]) {
+				case ">view":
+					switch (args[1]) {
+						case undefined:
+							console.log(Math.floor(Math.random() * images.length), images);
+							message.channel.send(images[Math.floor(Math.random() * images.length)].URL);
+							break;
+					}
 					break;
-				}
-			break;
+			}
+		} else {
+			switch (args[0]) {
+				case ">pic":
+					pictureSave(message, args, images);
+					break;
+			}
 		}
-	} else {
-		switch(args[0]) {
-			case ">pic":
-				pictureSave(message, args);
-			break;
-		}
-	}
+	});
+	
 });
 
-function pictureSave(message, args) {
-switch(args[1]) {
-					case "add":
-					var imgtitle = args[2];
-					var imgurl = args[3];
-					var retrievedImage;
-						fs.readFile('allImages.json', (err, data) => {
-							retrievedImage = JSON.parse(data);
-							retrievedImage.push('[{"ID": ' + currentID + ',"rating": 0, "title": "' + imgtitle + '", "URL": "' + imgurl + '", "server": 1}]');
-							fs.writeFile(idName, JSON.stringify(retrievedImage), err => {
-								throw err;
-							});
-						currentID++;
-						idName = "imgID-" + currentID;
-						writeMax();
-						message.channel.send("Image " + args[2] + " saved!");
-						});
-					break
-				}
+function pictureSave(message, args, images) {
+	switch (args[1]) {
+		case "add":
+			var imgtitle = args[2];
+			var imgurl = args[3];
+			var images;
+			
+			images.push({
+				rating: 0,
+				title: imgtitle,
+				URL: imgurl, server: 1
+			});
+			fs.writeFile('allImages.json', JSON.stringify(images), err => {
+				if (err) throw err;
+			});
+			message.channel.send("Image " + args[2] + " saved!");
+			break;
+	}
 }
 
