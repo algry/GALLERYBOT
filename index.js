bd30cd95dd8d5cf0188df0cbf9346c8f2c184991
algry <irongeardragon135@gmail.com>
2018-05-11 09:38:31 -0600

    fix bugs

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const discord = require("discord.js");
 const config = require("./config.json");
 const fs = require("fs");
-const client = new Discord.Client();
+const client = new discord.Client();
+const maxPost = require("./maxPost.json");
+const imgs = require("./allImages.json");
 
-var currentID = 1;
+var currentID = maxPost.maxPostNum;
 var idName = "imgID-" + currentID;
+
+function writeMax() {
+	fs.writeFile("MaxPost.json", '{"maxPostNum": ' + currentID + '}', "utf8", (err)=>{
+		if (err) {
+			throw err;
+		}
+		currentID++;
+		idName = "imgID-" + currentID;
+	});
+}
 
 var session = {
 	active: false
@@ -19,31 +31,45 @@ client.on('ready', () => {
   });
 });
 
-client.on('message' () => {
-	var args = message.content.splice("~");
+client.on('message', message => {
+	var args = message.content.split("~");
 	
+	if(message.author.bot) return;
 	if (message.channel.type === 'dm') {
-		
+		console.log(message.contents);
+		switch(args[0]) {
+			case ">view":
+				switch(args[1]) {
+					case "":
+						channel.send("");
+					break;
+				}
+			break;
+		}
 	} else {
 		switch(args[0]) {
 			case ">pic":
-				pictureSave();
-				break;
+				switch(args[1]) {
+					case "add":
+					var imgtitle = args[2];
+					var imgurl = args[3];
+					var retrievedImage;
+						fs.readFile('allImages.json', (err, data) => {
+							retrievedImage = JSON.parse(data);
+							retrievedImage[idName] = '{"ID": ' + currentID + ',"rating": 0, "title": "' + imgtitle + '", "URL": "' + imgurl + '", "server": 1}';
+							fs.writeFile(name, JSON.stringify(images), err => {
+								throw err;
+							});
+						);
+						currentID++;
+						idName = "imgID-" + currentID;
+						writeMax();
+						channel.send("Image " + args[2] + " saved!");
+						});
+					break
+				}
+			break;
 		}
 	}
 });
-
-pictureSave() {
-	switch(args[1]) {
-		case "add":
-			fs.writeFile(idName, '{"ID": ' + currentID + ',"rating": 0, "title": "' + args[3] '", "URL":' + args[2] '}', "utf8", (err) => {
-				if (err) {
-					throw err;
-				}
-				currentID++;
-				idName = "imgID-" + currentID;
-				})
-			break;
-	}
-};
 
